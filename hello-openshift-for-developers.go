a875076e76a84add81c9e5f94bb62120673f8b78
fedeos <54009273+fedeos@users.noreply.github.com>
2022-01-17 15:42:00 -0300

    Update hello-openshift-for-developers.go

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello New World!"
+		response = "Hello New World Openshift!!!"
 	}
 
 	fmt.Fprintln(w, response)
